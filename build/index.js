c39267c4e26f9433ea22e9e7b8f6b5b0f8960fc2
jonascolas <jonas@fusemachines.com>
2022-05-26 15:08:09 -0300

    match reader with abstract class

--- build/index.js
+++ build/index.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-console.log(reader.data[7]);
+//Create an object that satisfies the DataReader interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+//Create an instance of MatchReader and pass in something that satisfies the DataReader interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
