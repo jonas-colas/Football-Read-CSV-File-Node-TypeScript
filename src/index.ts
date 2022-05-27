6d27cd482e8ce02f6b2fc99af721806362c924c5
jonascolas <jonas@fusemachines.com>
2022-05-26 22:55:09 -0300

    refactoring code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-//Create an object that satisfies the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
 
-//Create an instance of MatchReader and pass in something that satisfies the DataReader interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCsv("football.csv");
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
