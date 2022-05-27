ea0fc15553cd2c0b29c6438b045df727cb664315
jonascolas <jonas@fusemachines.com>
2022-05-26 22:36:34 -0300

    composition vs inheritance

--- build/index.js
+++ build/index.js
@@ -2,20 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
+const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+const Summary_1 = require("./Summary");
 //Create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 //Create an instance of MatchReader and pass in something that satisfies the DataReader interface
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-// matchReader.matches
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
