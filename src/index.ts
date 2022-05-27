f8570a259de2105b5f4d7cb112807e89662cddf6
jonascolas <jonas@fusemachines.com>
2022-05-26 22:42:03 -0300

    write in file wi fs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { MatchReader } from "./MatchReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 //Create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -13,7 +14,7 @@ matchReader.load();
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport(),
+  new HtmlReport(),
 );
 
 summary.buildAndPrintReport(matchReader.matches);
